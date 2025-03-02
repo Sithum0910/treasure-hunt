bd5ae10a422afd9c18fc57d1e5bbc4f23a46f5f0
sithum kaushalya <91521402+Sithum0910@users.noreply.github.com>
2025-03-02 20:49:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function startHunt() {
+  // Play background music
+  const music = document.getElementById("background-music");
+  music.play();
+
+  // Redirect to the first clue
+  window.location.href = "clue1.html";
+}
